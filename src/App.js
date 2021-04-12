6bfbfb4f801e1426cca5cfd9c25e8b1163424ab0
Adrian Kowalski <adriann.kowalski@gmail.com>
2021-04-12 21:37:41 +0200

    v 1.3
    - Update buttons actions on list items
    - VideoCard styling
    - Video playback componentand with actions
    - ListItems styling and buttons actions
    - Video services icons in list display 
    - Pagination

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import React, { useState, useEffect } from 'react';
-import { Col, Row } from 'reactstrap';
+import { Col, Row, Container } from 'reactstrap';
+import { onConfirm } from 'react-confirm-pro';
 import './App.css';
 import { InputForm, NavButtons, VideoWrapper } from './components'
 import { GetByIdYouTube } from './apis'
 import { GetByIdVimeo } from './apis'
+import { Pagination } from './components/Pagination'
 
 const App = () => {
 const savedVideos = JSON.parse(localStorage.getItem('urlsStore'))
 
   const [urls , setUrls ] = useState(savedVideos || [])
   const [display, setDisplay] = useState([])
+  const [dispBackUp, setDispBackUp] = useState([])
+  const [favsDisplay, setFavsDisplay ] = useState(false)
+  const [sort, setSort] = useState(false)
+  const [displayType, setDisplayType] = useState(false)
+  const [currentPage, setCurrentPage] = useState(1)
+  const [videosPerPage] = useState(6)
+ 
 
 useEffect(() => {
   FetchData(urls)
  }, [urls])
 
-
  const FetchData = async (urls) =>{
    const newArr = []
 
   for (const item of urls) {
+     
        const url = item.url
        let ID =''
        let response = ''
@@ -37,56 +46,111 @@ useEffect(() => {
         ID = url.slice(url.search("com", 0) + 4, url.length)
         response = GetByIdVimeo(ID);
        }
-       
        const data = await response;
        const date = item.date;
-       const newItem = { data, url, date, ID, favs };
-       newArr.push(newItem)
-    
+       const newItem = { data, url, date, ID, favs }
+      
+     newArr.push(newItem)
+       
 }
-
 setDisplay(newArr)
-
 };
 
 const DeleteFromList = (date) =>{
- const urlsList = urls.filter(el => el.date !== date) // filtruj urls/ wez wszystkie "el" zwróc => wszystkie z ID które nie równa się ID wchodzącemu
+ const urlsList = urls.filter(el => el.date !== date)
  setUrls(urlsList)
  localStorage.clear()
  window.localStorage.setItem('urlsStore', JSON.stringify(urlsList))
 };
 
 const UpdateFavs = (date) =>{
-  const clickedIndex = urls.findIndex((el) => (el.date === date))// tablica z klikniętym obiektem
+  const clickedIndex = urls.findIndex((el) => (el.date === date))
   let newAr = [...urls]
   newAr[clickedIndex] = {...newAr[clickedIndex], favs: !newAr[clickedIndex].favs}
 
-setUrls(newAr)
+  setUrls(newAr)
+  localStorage.clear()
+  window.localStorage.setItem('urlsStore', JSON.stringify(newAr))
+};
 
-localStorage.clear()
-window.localStorage.setItem('urlsStore', JSON.stringify(newAr))
+const FavoritesToggle = () =>{
+  setFavsDisplay(!favsDisplay)
+const favsArr = display.filter( item => item.favs === true)
+setDispBackUp(display)
+ favsDisplay ? setDisplay(dispBackUp) : setDisplay(favsArr)
+};
 
-console.log(urls)
+const SortbyDate = () =>{
+  setSort(!sort)
+  display.reverse()
 }
 
+const DeleteAll = async () =>{
+  onConfirm({
+    title: (
+      <h3>
+        Are you sure?
+      </h3>
+    ),
+    description: (
+      <p>This process cannot be undone.</p>
+    ),
+    onSubmit: () => {
+      localStorage.clear()
+  setUrls([])
+  setDisplay([])
+    },
+    onCancel: () => {
+    },
+  })
+};
+
+const ChangeDisplay= () =>{
+setDisplayType(!displayType)
+};
+
+const indexOfLastItem = currentPage * videosPerPage;
+const indexOfFirstItem = indexOfLastItem - videosPerPage;
+const currentItems = display.slice(indexOfFirstItem, indexOfLastItem)
+
+const paginate = (pageNumber) => setCurrentPage(pageNumber)
+
+// do wrappera przekaż tylko wycinek daty
+
   return (
-    <div className="App">
+   
+      <Container>
       <Col>
-        <Col class="row mx-md-n5">
-        <InputForm urls={urls} setUrls={setUrls}/>
-        </Col>
-         <Row >
           <Col>
-            <NavButtons />
+            <InputForm urls={urls} setUrls={setUrls}/>
           </Col>
-         </Row>
-        <VideoWrapper 
-        display={display}
-        OnDelete={DeleteFromList}
-        UpdateFavs={UpdateFavs}
-        />
+            <Row >
+              <Col>
+                <NavButtons 
+                  FavsToggle={FavoritesToggle}
+                  FavsDisplay={favsDisplay}
+                  SortbyDate={SortbyDate}
+                  Sort={sort}
+                  DeleteAll={DeleteAll}
+                  ChangeDisplay={ChangeDisplay}
+                  displayType={displayType}
+                />
+              </Col>
+              <Pagination videosPerPage={videosPerPage} totalVideos={display.length} paginate={paginate} currentPage={currentPage}/>
+            </Row>
+         <VideoWrapper 
+          display={currentItems}
+          displayType={displayType}
+          OnDelete={DeleteFromList}
+          UpdateFavs={UpdateFavs}
+          favsDisplay={FavoritesToggle}
+         />
+        
       </Col>
-    </div>
+      </Container>
+      
+      
+     
   );
 }
 
