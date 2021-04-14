f376b569ee6a29b3e45ef19e9042bc88d670b07b
Adrian Kowalski <adriann.kowalski@gmail.com>
2021-04-14 21:01:04 +0200

    v 1.6
    - Load ID’s from an external file 
    - Update delete function
    - Mobile screen adjustments
    - More styling

--- src/App.js
+++ src/App.js
@@ -1,78 +1,68 @@
 import React, { useState, useEffect } from 'react';
-import { Col, Row, Container } from 'reactstrap';
+import { Container } from 'reactstrap';
 import { onConfirm } from 'react-confirm-pro';
-import './App.css';
 import { InputForm, NavButtons, VideoWrapper } from './components'
 import { GetByIdYouTube } from './apis'
 import { GetByIdVimeo } from './apis'
-import { Pagination } from './components/Pagination'
+import { Pagination } from './components'
+import './App.css';
 
 const App = () => {
 const savedVideos = JSON.parse(localStorage.getItem('urlsStore'))
-
-  const [urls , setUrls ] = useState(savedVideos || [])
-  const [display, setDisplay] = useState([])
-  const [dispBackUp, setDispBackUp] = useState([])
-  const [favsDisplay, setFavsDisplay ] = useState(false)
-  const [sort, setSort] = useState(false)
-  const [displayType, setDisplayType] = useState(false)
-  const [currentPage, setCurrentPage] = useState(1)
-  const [videosPerPage] = useState(6)
+const [urls , setUrls ] = useState(savedVideos || [])
+const [display, setDisplay] = useState([])
+const [dispBackUp, setDispBackUp] = useState([])
+const [favsDisplay, setFavsDisplay ] = useState(false)
+const [sort, setSort] = useState(false)
+const [displayType, setDisplayType] = useState(false)
+const [currentPage, setCurrentPage] = useState(1)
+const [videosPerPage] = useState(6)
  
-
 useEffect(() => {
   FetchData(urls)
+  localStorage.clear()
+  window.localStorage.setItem('urlsStore', JSON.stringify(urls))
  }, [urls])
 
  const FetchData = async (urls) =>{
-   const newArr = []
-   
-  for (const item of urls) {
-     
+  const newArr = []
+   for (const item of urls) {
        const url = item.url
        let ID =''
        let response = ''
        let favs = item.favs
        let isnum = /^\d+$/.test(url);
 
-       // NOW:
-// DLA YT sprawdza czy zawiera youtu i jest dłuższy niż 30 lub krótszy niż 30 długi i krótki link
-// wystarczyło by dodać na początku jeśli url.length < 13 i zawiera literę lub _ to procesujemy YT fetchem
-// jeśli nie zawiera same cyfry procesujemy vimeo fetchem
-
-      if (url.length < 13 && isnum) {
+  if (url.length < 13 && isnum) {
         ID = url
         response = GetByIdVimeo(ID);
 
-      } else if (url.length < 13 && !isnum) {
+  } else if (url.length < 13 && !isnum) {
         ID = url
         response = GetByIdYouTube(ID)
-      } else if (url.includes("youtu") && url.length > 30) {
+  } else if (url.includes("youtu") && url.length > 30) {
         ID = url.slice(url.search("=", 0) + 1, url.length);
         response = GetByIdYouTube(ID);
 
-
-       } else if (url.includes("youtu") && url.length < 30) {
+  } else if (url.includes("youtu") && url.length < 30) {
          ID = url.slice(url.search(".be", 0) + 4, url.length)
          response = GetByIdYouTube(ID);
 
-       } else if (url.includes("vimeo",0)) {
+  } else if (url.includes("vimeo",0)) {
         ID = url.slice(url.search("com", 0) + 4, url.length)
         response = GetByIdVimeo(ID);
        }
        const data = await response;
        const date = item.date;
-       const newItem = { data, url, date, ID, favs }
-      
-     newArr.push(newItem)
-       
+       const key = parseInt(ID, 36) * Date.now()
+       let newItem = { data, url, date, ID, favs, key }
+     newArr.push(newItem)   
 }
 setDisplay(newArr)
-console.log(display)
 };
 
-const DeleteFromList = (date) =>{
- const urlsList = urls.filter(el => el.date !== date)
+const DeleteFromList = (key) =>{
+ const urlsList = display.filter(el => el.key !== key)
  setUrls(urlsList)
  localStorage.clear()
  window.localStorage.setItem('urlsStore', JSON.stringify(urlsList))
@@ -82,7 +72,6 @@ const UpdateFavs = (date) =>{
   const clickedIndex = urls.findIndex((el) => (el.date === date))
   let newAr = [...urls]
   newAr[clickedIndex] = {...newAr[clickedIndex], favs: !newAr[clickedIndex].favs}
-
   setUrls(newAr)
   localStorage.clear()
   window.localStorage.setItem('urlsStore', JSON.stringify(newAr))
@@ -98,7 +87,7 @@ setDispBackUp(display)
 const SortbyDate = () =>{
   setSort(!sort)
   display.reverse()
-}
+};
 
 const DeleteAll = async () =>{
   onConfirm({
@@ -130,44 +119,31 @@ const currentItems = display.slice(indexOfFirstItem, indexOfLastItem)
 
 const paginate = (pageNumber) => setCurrentPage(pageNumber)
 
-// do wrappera przekaż tylko wycinek daty
-
   return (
-   
-      <Container>
-      <Col>
-          <Col>
-            <InputForm urls={urls} setUrls={setUrls}/>
-          </Col>
-            <Row >
-              <Col>
-                <NavButtons 
-                  FavsToggle={FavoritesToggle}
-                  FavsDisplay={favsDisplay}
-                  SortbyDate={SortbyDate}
-                  Sort={sort}
-                  DeleteAll={DeleteAll}
-                  ChangeDisplay={ChangeDisplay}
-                  displayType={displayType}
-                />
-              </Col>
-              <Pagination videosPerPage={videosPerPage} totalVideos={display.length} paginate={paginate} currentPage={currentPage}/>
-            </Row>
-         <VideoWrapper 
-          display={currentItems}
-          displayType={displayType}
-          OnDelete={DeleteFromList}
-          UpdateFavs={UpdateFavs}
-          favsDisplay={FavoritesToggle}
-         />
-        
-      </Col>
+      <Container id='mainCnt'>
+         <InputForm urls={urls} setUrls={setUrls}/>
+            <NavButtons
+             urls={urls}
+             setUrls={setUrls}
+             FavsToggle={FavoritesToggle}
+             FavsDisplay={favsDisplay}
+             SortbyDate={SortbyDate}
+             Sort={sort}
+             DeleteAll={DeleteAll}
+             ChangeDisplay={ChangeDisplay}
+             displayType={displayType}
+             FetchData={FetchData}
+             />
+               <Pagination videosPerPage={videosPerPage} totalVideos={display.length} paginate={paginate} currentPage={currentPage}/>
+             <VideoWrapper 
+             display={currentItems}
+             displayType={displayType}
+             OnDelete={DeleteFromList}
+             UpdateFavs={UpdateFavs}
+             favsDisplay={FavoritesToggle}
+            />
       </Container>
-      
-      
-     
   );
 }
-
 
 export default App;
